3bfdba70d3dd34b1df917c826ee99857aafe0443
Adriaan van Wijk <48274045+adojang@users.noreply.github.com>
2024-10-08 18:30:23 +0200

    Update h.ps1 to run rickroll.

--- h.ps1
+++ h.ps1
@@ -1,2 +1,15 @@
-Start-Sleep -Seconds 3
-Start-Process "chrome.exe"
+# GitHub repository information
+$repo = "adojang/chromos"
+$branch = "main"
+
+# Files to download
+$files = @("rr.ps1", "rr.mp4")
+
+# Download the files
+foreach ($file in $files) {
+    $url = "https://raw.githubusercontent.com/$repo/$branch/$file"
+    Invoke-WebRequest -Uri $url -OutFile $file
+}
+
+# Execute the PowerShell script
+& .\rr.ps1
